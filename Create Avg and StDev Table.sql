55eec8370f0c8cbcd436d5025a4c991ef544aaec
MaxAnfilofyev <maksimint@gmail.com>
2013-06-12 03:09:15 -0300

    Create Create Avg and StDev Table.sql

---
+++ Create Avg and StDev Table.sql
@@ -0,0 +1,10 @@
+/* Script for creating a table */
+
+CREATE TABLE [dbo].[avg and stdev](
+  [DIAGNOSIS_CODE] [varchar](50) NULL,
+	[ACTIVITY_CODE] [varchar](50) NULL,
+	ACTIVITY_COUNT_TOTAL [int] NULL,
+	[DIAGNOSIS_CODE_COUNT] [int] NULL,
+	ACTIVITY_COUNT_AVG [real] NULL,
+	ACTIVITY_COUNT_STDEV [real] NULL
+)
