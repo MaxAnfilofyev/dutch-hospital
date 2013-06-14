2b2dc08e3a97c86587d945bab5e9adb128446deb
MaxAnfilofyev <maksimint@gmail.com>
2013-06-14 13:40:23 -0300

    Update Create Avg and StDev Table.sql
    added CareProfile_class_code

--- Create Avg and StDev Table.sql
+++ Create Avg and StDev Table.sql
@@ -1,11 +1,12 @@
 /* Script for creating a table */
 
 CREATE TABLE [dbo].[avg and stdev](
-  	Specialist [varchar](50) NULL,
-  	[DIAGNOSIS_CODE] [varchar](50) NULL,
+	[Specialist] [varchar](50) NULL,
+	[DIAGNOSIS_CODE] [varchar](50) NULL,
 	[ACTIVITY_CODE] [varchar](50) NULL,
-	ACTIVITY_COUNT_TOTAL [int] NULL,
+	[ACTIVITY_COUNT_TOTAL] [int] NULL,
 	[DIAGNOSIS_CODE_COUNT] [int] NULL,
-	ACTIVITY_COUNT_AVG [real] NULL,
-	ACTIVITY_COUNT_STDEV [real] NULL
+	[ACTIVITY_COUNT_AVG] [real] NULL,
+	[ACTIVITY_COUNT_STDEV] [real] NULL,
+	[CareProfile_class_code] [varchar](50) NULL
 )
